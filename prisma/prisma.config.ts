cfbfccf3dd781f7e82640eb21663552e76cd5e34
sleddd <sledddd@gmail.com>
2025-12-30 01:20:53 -0800

    Updating build script to include Prisma commands

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -4,7 +4,6 @@ import { defineConfig, env } from 'prisma/config'
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    // Looks for both common names automatically
     url: env('PRISMA_DATABASE_URL') || env('DATABASE_URL'),
   },
 })
